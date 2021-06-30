5736356610a241338163d702976e8fbc29b8dc8a
Cameron <cameron_uniac@hotmail.com>
2021-06-30 18:04:25 -0400

    Created logic for vertical word searching, and added test case for vertical searching as well

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  
+  let tempArray = [];
+  for (let string in letters[0]) {
+    tempArray.push([]);
+  }
+  
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      tempArray[j].push(letters[i][j]);
     }
+  }
+  
+  const verticalJoin = tempArray.map(tempArray => tempArray.join(''));
+
+  for (horizontalString of horizontalJoin) {
+    if (horizontalString.includes(word)) {
+      return true
+    }
+  }
+  
+  for (verticalString of verticalJoin) {
+    if (verticalString.includes(word)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = wordSearch
+
